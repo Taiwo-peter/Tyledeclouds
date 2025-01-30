8d50bcd4881ce211c5bbef724d24196c1f0ea283
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-01-30 14:46:01 +0000

    APP AND DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-echo "Starting MySQL server..."
-exec mysqld
+
+# Ensure MySQL data directory is initialized
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing database..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+fi
+
+# Start MariaDB service in the background
+exec mysqld --user=mysql --datadir=/var/lib/mysql
