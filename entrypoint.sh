15aab06e73df3ae9598d6fdedceeb89173a38a79
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-01-30 23:09:36 +0000

    APP AND DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-# Initialize database if not already initialized
+# Initialize database if not exists
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+  echo "Initializing database..."
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
 # Start MariaDB
