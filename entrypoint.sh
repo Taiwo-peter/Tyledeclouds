8f8bf635a1586138a2d3cc21ac67072346da409f
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-01-30 23:48:18 +0000

    APP AND DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Initialize database if not exists
+# Initialize database
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
   
@@ -11,12 +11,12 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
   # Wait for DB to start
   timeout 30s bash -c 'until mysqladmin ping -u root --silent; do sleep 1; done'
   
-  # Secure installation
+  # Configure database
   mysql -u root <<-EOSQL
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    DELETE FROM mysql.user WHERE User='';
-    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+    CREATE DATABASE ${MYSQL_DATABASE};
     CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
     FLUSH PRIVILEGES;
@@ -26,5 +26,5 @@ EOSQL
   mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
 
-# Start production instance
-exec mysqld_safe --user=mysql --datadir=/var/lib/mysql
+# Start production instance with TCP support
+exec mysqld_safe --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
