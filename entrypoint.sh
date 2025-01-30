9702607210cdd33e11a9c880c26d3501b4b3793c
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-01-30 23:51:47 +0000

    APP AND DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-# Initialize database
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
-  
-  # Start temporary DB instance
   mysqld_safe --user=mysql --datadir=/var/lib/mysql --skip-networking &
-  
-  # Wait for DB to start
   timeout 30s bash -c 'until mysqladmin ping -u root --silent; do sleep 1; done'
   
-  # Configure database
   mysql -u root <<-EOSQL
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
@@ -21,10 +15,8 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
-  
-  # Shutdown temporary instance
+
   mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
 
-# Start production instance with TCP support
 exec mysqld_safe --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
